8d5a8b54c41e3d9c436efc88ae045e442cf67c60
Lilian <liliankasem@gmail.com>
2018-05-14 15:00:49 -0700

    remove clean, corrected skip ci to ci skip

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -29,7 +29,7 @@ make_version() {
   
   # Run the deploy build and increment the package versions
   # %s is the placeholder for the created tag
-  npm version patch -m "chore: release version %s [skip ci]"
+  npm version patch -m "chore: release version %s [ci skip]"
 }
 
 upload_files() {
