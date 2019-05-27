58de156810e13a7341bcda1673e01e017079a61f
Andrew Craswell <andcra@microsoft.com>
2019-05-27 10:30:20 -0700

    Export necessary MSAL interfaces/classes

--- src/index.tsx
+++ src/index.tsx
@@ -23,10 +23,13 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+import { Account, AuthenticationParameters, CacheLocation, Configuration, UserAgentApplication } from 'msal';
+
 import { AAD_LOGIN_SUCCESS, AAD_LOGOUT_SUCCESS } from './actions';
 import { AzureAD } from './AzureAD'
 import { AuthenticationState, IAccountInfo, LoginType } from './Interfaces';
 import { MsalAuthProviderFactory } from './MsalAuthProviderFactory';
 
+export { Account, AuthenticationParameters, CacheLocation, Configuration, UserAgentApplication };
 export { AzureAD, AAD_LOGIN_SUCCESS, AAD_LOGOUT_SUCCESS, AuthenticationState, IAccountInfo, LoginType, MsalAuthProviderFactory };
 export default AzureAD;
