0ad5b55a6660a0317fb95da019864501d0cc94b3
Andrew Craswell <andcra@microsoft.com>
2019-12-10 13:45:18 -0800

    Produce ES6 modules, ES5 CommonJS, and UMD builds (#175)
    * Update to MSAL version 1.2.0 and fix breaking changes
    
    * Hotfix for edge case where MSAL exceptions in the token refresh iframe can cause infinite auth
    
    * Remove license from source files
    
    * Add builds for CommonJS, UMD, and ES Modules
    
    * Change the target output of the ES Modules build to ES6
    
    * Migrate to ESLint from TSLint
    
    * Remove leagal banner from all sample source files
    
    * Update the editor config and vscode settings to match closer with MSAL
    
    * Update docs to fix missing mentions of the new MsalAuthProvider options parameter
    
    * Fix unit tests

--- src/MsalAuthProvider.ts
+++ src/MsalAuthProvider.ts
@@ -1,27 +1,3 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
-// MIT License:
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
 import {
   AuthenticationParameters,
   AuthError,
@@ -55,6 +31,7 @@ export class MsalAuthProvider extends UserAgentApplication implements IAuthProvi
 
   /**
    * Gives access to the MSAL functionality for advanced usage.
+   *
    * @deprecated The MsalAuthProvider class itself extends from UserAgentApplication and has the same functionality
    */
   public UserAgentApplication: UserAgentApplication;
@@ -102,7 +79,7 @@ export class MsalAuthProvider extends UserAgentApplication implements IAuthProvi
     if (this._options.loginType === LoginType.Redirect) {
       this.setAuthenticationState(AuthenticationState.InProgress);
       try {
-        await this.loginRedirect(params);
+        this.loginRedirect(params);
       } catch (error) {
         Logger.ERROR(error);
 
@@ -289,11 +266,11 @@ export class MsalAuthProvider extends UserAgentApplication implements IAuthProvi
     }
   };
 
-  private authenticationRedirectCallback = async (error: AuthError, response: AuthResponse) => {
+  private authenticationRedirectCallback = (error: AuthError) => {
     if (error) {
       this.setError(error);
     }
-    await this.processLogin();
+    this.processLogin();
   };
 
   private initializeProvider = async () => {
--- src/logger.ts
+++ src/logger.ts
@@ -1,43 +1,21 @@
-// tslint:disable: no-console
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
-// MIT License:
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-
 export class Logger {
   public static VERBOSE(message: string, ...optionalParams: any[]) {
-    console.log.apply(console, ['[VERBOSE] ' + message].concat(optionalParams));
+    // eslint-disable-next-line no-console
+    console.log(...['[VERBOSE] ' + message].concat(optionalParams));
   }
 
   public static INFO(message: string, ...optionalParams: any[]) {
-    console.info.apply(console, ['[INFO] ' + message].concat(optionalParams));
+    // eslint-disable-next-line no-console
+    console.info(...['[INFO] ' + message].concat(optionalParams));
   }
 
   public static WARN(message: string, ...optionalParams: any[]) {
-    console.warn.apply(console, ['[WARN] ' + message].concat(optionalParams));
+    // eslint-disable-next-line no-console
+    console.warn(...['[WARN] ' + message].concat(optionalParams));
   }
 
   public static ERROR(message: string, ...optionalParams: any[]) {
-    console.error.apply(console, ['[ERROR] ' + message].concat(optionalParams));
+    // eslint-disable-next-line no-console
+    console.error(...['[ERROR] ' + message].concat(optionalParams));
   }
 }
