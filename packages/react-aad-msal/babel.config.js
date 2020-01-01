1bb3daf990566b8402dbc970b3f513ec93bd4304
Andrew Craswell <andcra@gmail.com>
2019-12-31 18:04:30 -0800

    Fix unit tests

--- packages/react-aad-msal/babel.config.js
+++ packages/react-aad-msal/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-react', '@babel/preset-typescript'],
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
   plugins: ['@babel/proposal-class-properties', '@babel/plugin-syntax-dynamic-import'],
   comments: false,
 };
